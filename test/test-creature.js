7f13b559e83973b81c1819d0877ceeda803c8442
Saiju <saijumathew@gmail.com>
2021-01-24 21:46:30 -0500

    reverting changes

--- test/test-creature.js
+++ test/test-creature.js
@@ -27,7 +27,7 @@ contract("Creature", accounts => {
           .then(name => {
               assert.equal(
               name,
-              "Creature-SMS2",
+              "Creature-SMS",
               "name is not creature"
               );
           });
